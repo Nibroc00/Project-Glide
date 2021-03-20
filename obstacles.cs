c44bf932268d1e5b78b2a70170f32d772c39c3ca
Trent Esplin <trent.esplin@students.snow.edu>
2021-03-20 13:33:31 -0600

    Testing full compilation

--- obstacles.cs
+++ obstacles.cs
@@ -1,9 +1,18 @@
 ﻿using System;
-
-public class obstacles
+using System.Collections.Generic;
+using System.Text;
+namespace groundObstacle
 {
-	public obstacles()
+	public class obstacles
 	{
-		Console.WriteLine("This is a test!")
+		//An obstacle ("l") will have a random chance to spawn every tick after 6 ticks from the last obstacle. 
+		//If no obstacle has spawned yet then it can randomly spawn immediatly. 
+		//This will be done when an obstacle can spawn on the right of the grid view and move to the left at tick speed.
+
+		int designChoice = 223;
+		public obstacles()
+		{
+			System.Console.WriteLine("{0} = {1}", designChoice, (char)designChoice);
+		}
 	}
 }
